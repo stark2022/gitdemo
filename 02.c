9c91fde975a09f2d9efcf762b35f6e85ae9c4a4d
stark2022 <stark2022@163.com>
2022-06-11 11:16:21 +0800

    gitdemo

---
+++ 02.c
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: wyzf
+ * @Date: 2022-06-10 16:12:01
+ * @LastEditors: wyzf
+ * @LastEditTime: 2022-06-10 16:14:05
+ */
+#include <stdio.h>
+
+int main()
+{
+    /* 我的第一个 C 程序 */
+    printf("Hello, World! \n");
+
+    return 0;
+}
---
+++ 03.py
@@ -0,0 +1,9 @@
+'''
+Descripttion: 
+version: 
+Author: wyzf
+Date: 2022-06-10 16:12:01
+LastEditors: wyzf
+LastEditTime: 2022-06-11 09:24:13
+'''
+print("hello world!")
