cfd874232fce9868daab4d4714481eb58e23b371
stark2022 <stark2022@163.com>
2022-06-11 13:29:04 +0800

    printf

--- 02.c
+++ 02.c
@@ -4,13 +4,14 @@
  * @Author: wyzf
  * @Date: 2022-06-10 16:12:01
  * @LastEditors: wyzf
- * @LastEditTime: 2022-06-10 16:14:05
+ * @LastEditTime: 2022-06-11 13:27:54
  */
 #include <stdio.h>
 
 int main()
 {
     /* 我的第一个 C 程序 */
+    printf("Hello, World! \n");
     printf("Hello, World! \n");
 
     return 0;
